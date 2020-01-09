15e80cf776a47e06a76ef03bfb556ce082509378
Andrii Siriak <siryaka@gmail.com>
2020-01-09 10:40:20 +0200

    Set up initial files (#1)
    * Add initial structure
    
    * Add todo
    
    Co-authored-by: Adelhaide <35349311+Adelhaide22@users.noreply.github.com>

---
+++ testforms/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Core;
+
+namespace testforms
+{
+    internal class MainForm : Form
+    {       
+        private Button btn;
+        private ListBox rtb;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void InitializeComponent()
+        {
+            Text = "Money Manager";
+
+            Size = new Size(1000, 800);            
+            btn = new Button { Text = "Submit", Location = new Point(10, 100) };
+            rtb = new ListBox { Text = "Submit", Location = new Point(10, 150), Width = 1000, Height = 500 };
+            btn.Click += Btn_Click;
+
+            Controls.Add(btn);
+            Controls.Add(rtb);
+        }
+        
+        async void Btn_Click(object sender, EventArgs e)
+        {
+            
+        }       
+
+        private void RenderCategory(Index index)
+        {
+            // max and min index
+            // changes for average in month
+        }
+    }
+}
---
+++ testforms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace testforms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Application.Run(new MainForm());
+        }
+    }
+}
