81cfd85275952a675f35dd6e66a5a793c9ee456a
Alina Siriak <adelhaide22@gmail.com>
2020-01-27 22:12:13 +0200

    Combine forms

--- WinFormsUI/MainForm.Designer.cs
+++ WinFormsUI/MainForm.Designer.cs
@@ -28,60 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnOpenChart = new System.Windows.Forms.Button();
-            this.btnOpenTransactionsForm = new System.Windows.Forms.Button();
-            this.btnQuit = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lbTransactions = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.chartSeries = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSeries)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnOpenChart
+            // tabControl1
             // 
-            this.btnOpenChart.Location = new System.Drawing.Point(201, 23);
-            this.btnOpenChart.Name = "btnOpenChart";
-            this.btnOpenChart.Size = new System.Drawing.Size(75, 23);
-            this.btnOpenChart.TabIndex = 0;
-            this.btnOpenChart.Text = "Open Chart";
-            this.btnOpenChart.UseVisualStyleBackColor = true;
-            this.btnOpenChart.Click += new System.EventHandler(this.btnOpenChart_Click);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(284, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(512, 425);
+            this.tabControl1.TabIndex = 3;
             // 
-            // btnOpenTransactionsForm
+            // tabPage1
             // 
-            this.btnOpenTransactionsForm.Location = new System.Drawing.Point(30, 23);
-            this.btnOpenTransactionsForm.Name = "btnOpenTransactionsForm";
-            this.btnOpenTransactionsForm.Size = new System.Drawing.Size(111, 23);
-            this.btnOpenTransactionsForm.TabIndex = 1;
-            this.btnOpenTransactionsForm.Text = "Transactions";
-            this.btnOpenTransactionsForm.UseVisualStyleBackColor = true;
-            this.btnOpenTransactionsForm.Click += new System.EventHandler(this.btnOpenTransactionsForm_Click);
+            this.tabPage1.Controls.Add(this.lbTransactions);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(504, 399);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "List";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // btnQuit
+            // lbTransactions
             // 
-            this.btnQuit.Location = new System.Drawing.Point(577, 23);
-            this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(75, 23);
-            this.btnQuit.TabIndex = 2;
-            this.btnQuit.Text = "Quit";
-            this.btnQuit.UseVisualStyleBackColor = true;
-            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            this.lbTransactions.FormattingEnabled = true;
+            this.lbTransactions.Location = new System.Drawing.Point(20, 13);
+            this.lbTransactions.Name = "lbTransactions";
+            this.lbTransactions.Size = new System.Drawing.Size(463, 368);
+            this.lbTransactions.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chartSeries);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(504, 399);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Graphics";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePickerEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(31, 109);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerEnd.TabIndex = 14;
+            this.dateTimePickerEnd.ValueChanged += new System.EventHandler(this.dateTimePickerEnd_ValueChanged);
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.Checked = false;
+            this.dateTimePickerStart.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePickerStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerStart.Location = new System.Drawing.Point(31, 55);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerStart.TabIndex = 13;
+            this.dateTimePickerStart.Value = new System.DateTime(2020, 1, 19, 15, 10, 57, 0);
+            this.dateTimePickerStart.ValueChanged += new System.EventHandler(this.dateTimePickerStart_ValueChanged);
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Location = new System.Drawing.Point(28, 93);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(50, 13);
+            this.lblEndDate.TabIndex = 12;
+            this.lblEndDate.Text = "End date";
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Location = new System.Drawing.Point(28, 39);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(53, 13);
+            this.lblStartDate.TabIndex = 11;
+            this.lblStartDate.Text = "Start date";
+            // 
+            // chartSeries
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartSeries.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartSeries.Legends.Add(legend2);
+            this.chartSeries.Location = new System.Drawing.Point(19, 42);
+            this.chartSeries.Name = "chartSeries";
+            this.chartSeries.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartSeries.Series.Add(series2);
+            this.chartSeries.Size = new System.Drawing.Size(461, 336);
+            this.chartSeries.TabIndex = 1;
+            this.chartSeries.Text = "chartSeries";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnQuit);
-            this.Controls.Add(this.btnOpenTransactionsForm);
-            this.Controls.Add(this.btnOpenChart);
+            this.ClientSize = new System.Drawing.Size(808, 466);
+            this.Controls.Add(this.dateTimePickerEnd);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.lblEndDate);
+            this.Controls.Add(this.dateTimePickerStart);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartSeries)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnOpenChart;
-        private System.Windows.Forms.Button btnOpenTransactionsForm;
-        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox lbTransactions;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSeries;
     }
 }
--- WinFormsUI/MainForm.cs
+++ WinFormsUI/MainForm.cs
@@ -1,36 +1,75 @@
 ﻿using System;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using Core;
 
 namespace WinFormsUI
 {
     public partial class MainForm : Form
     {
+        Date startDate = new Date(DateTime.Now.Year - 1, DateTime.Now.Month, DateTime.Now.Day);
+        Date endDate = new Date(DateTime.Now);
+
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void btnOpenTransactionsForm_Click(object sender, EventArgs e)
+        private async void MainForm_Load(object sender, EventArgs e)
         {
-            var transForm = new TransactionsForm();
-            Hide();
-            transForm.Show();
+            State.OnStateUpdated += RefreshList;
+            State.OnStateUpdated += RefreshChart;
 
-            transForm.FormClosed += (o, e) => Show();
+            dateTimePickerStart.Value = new DateTime(DateTime.Now.Year - 1, DateTime.Now.Month, DateTime.Now.Day);
+            dateTimePickerEnd.Value = DateTime.Now.Date;
+
+            await State.Init();
         }
 
-        private void btnOpenChart_Click(object sender, EventArgs e)
+        private void RefreshList()
         {
-            var timeSeriesForm = new TimeSeriesForm();
-            Hide();
-            timeSeriesForm.Show();
-
-            timeSeriesForm.FormClosed += (o, e) => Show();
+            lbTransactions.Invoke(new Action(() =>
+            {
+                lbTransactions.Items.Clear();
+                lbTransactions.Items.AddRange(State.Transactions.SkipWhile(t => t.TimeStamp.DateTime < startDate).TakeWhile(t => t.TimeStamp.DateTime <= endDate).Select(t => (object)$"{t.Amount.Amount} {t.Amount.Currency}: {t.Description}").Reverse().ToArray());
+            }));
         }
 
-        private void btnQuit_Click(object sender, EventArgs e) => Application.Exit();
+        private void RefreshChart()
+        {
+            chartSeries.Series.Clear();
+            var series = new Series
+            {
+                Name = "Time Series",
+                Color = Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Line
+            };
 
-        private async void MainForm_Load(object sender, EventArgs e) => await State.Init();
+            var timeSeries = State.GetTimeSeries("all", 0.99);
+            // TODO: Display chart using selected dates
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                series.Points.AddXY(date.ToString(), timeSeries[date]);
+            }
+            chartSeries.Series.Add(series);
+        }        
+
+        private void dateTimePickerStart_ValueChanged(object sender, EventArgs e)
+        {
+            startDate = dateTimePickerStart.Value.ToDate();
+            RefreshList();
+            RefreshChart();
+        }
+
+        private void dateTimePickerEnd_ValueChanged(object sender, EventArgs e)
+        {
+            endDate = dateTimePickerEnd.Value.ToDate();
+            RefreshList();
+            RefreshChart();
+        }
     }
 }
