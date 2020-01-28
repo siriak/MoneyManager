9db7630ca34dc9047266a2e6be7325e69229218f
Alina Siriak <adelhaide22@gmail.com>
2020-01-28 13:44:48 +0200

    Rename controls

--- WinFormsUI/MainForm.Designer.cs
+++ WinFormsUI/MainForm.Designer.cs
@@ -28,44 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabList = new System.Windows.Forms.TabPage();
             this.lbTransactions = new System.Windows.Forms.ListBox();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabChart = new System.Windows.Forms.TabPage();
+            this.chartSeries = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
             this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
             this.lblEndDate = new System.Windows.Forms.Label();
             this.lblStartDate = new System.Windows.Forms.Label();
-            this.chartSeries = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.tabList.SuspendLayout();
+            this.tabChart.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartSeries)).BeginInit();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tabs
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(284, 29);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(512, 425);
-            this.tabControl1.TabIndex = 3;
+            this.tabs.Controls.Add(this.tabList);
+            this.tabs.Controls.Add(this.tabChart);
+            this.tabs.Location = new System.Drawing.Point(284, 29);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(512, 425);
+            this.tabs.TabIndex = 3;
             // 
-            // tabPage1
+            // tabList
             // 
-            this.tabPage1.Controls.Add(this.lbTransactions);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(504, 399);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "List";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabList.Controls.Add(this.lbTransactions);
+            this.tabList.Location = new System.Drawing.Point(4, 22);
+            this.tabList.Name = "tabList";
+            this.tabList.Padding = new System.Windows.Forms.Padding(3);
+            this.tabList.Size = new System.Drawing.Size(504, 399);
+            this.tabList.TabIndex = 0;
+            this.tabList.Text = "List";
+            this.tabList.UseVisualStyleBackColor = true;
             // 
             // lbTransactions
             // 
@@ -75,16 +75,33 @@
             this.lbTransactions.Size = new System.Drawing.Size(463, 368);
             this.lbTransactions.TabIndex = 1;
             // 
-            // tabPage2
+            // tabChart
             // 
-            this.tabPage2.Controls.Add(this.chartSeries);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(504, 399);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Graphics";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabChart.Controls.Add(this.chartSeries);
+            this.tabChart.Location = new System.Drawing.Point(4, 22);
+            this.tabChart.Name = "tabChart";
+            this.tabChart.Padding = new System.Windows.Forms.Padding(3);
+            this.tabChart.Size = new System.Drawing.Size(504, 399);
+            this.tabChart.TabIndex = 1;
+            this.tabChart.Text = "Chart";
+            this.tabChart.UseVisualStyleBackColor = true;
+            // 
+            // chartSeries
+            // 
+            chartArea.Name = "ChartArea";
+            this.chartSeries.ChartAreas.Add(chartArea);
+            legend.Name = "Legend";
+            this.chartSeries.Legends.Add(legend);
+            this.chartSeries.Location = new System.Drawing.Point(19, 42);
+            this.chartSeries.Name = "chartSeries";
+            this.chartSeries.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series.ChartArea = "ChartArea";
+            series.Legend = "Legend";
+            series.Name = "Series";
+            this.chartSeries.Series.Add(series);
+            this.chartSeries.Size = new System.Drawing.Size(461, 336);
+            this.chartSeries.TabIndex = 1;
+            this.chartSeries.Text = "chartSeries";
             // 
             // dateTimePickerEnd
             // 
@@ -126,39 +143,22 @@
             this.lblStartDate.TabIndex = 11;
             this.lblStartDate.Text = "Start date";
             // 
-            // chartSeries
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chartSeries.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chartSeries.Legends.Add(legend2);
-            this.chartSeries.Location = new System.Drawing.Point(19, 42);
-            this.chartSeries.Name = "chartSeries";
-            this.chartSeries.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chartSeries.Series.Add(series2);
-            this.chartSeries.Size = new System.Drawing.Size(461, 336);
-            this.chartSeries.TabIndex = 1;
-            this.chartSeries.Text = "chartSeries";
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(808, 466);
             this.Controls.Add(this.dateTimePickerEnd);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabs);
             this.Controls.Add(this.lblStartDate);
             this.Controls.Add(this.lblEndDate);
             this.Controls.Add(this.dateTimePickerStart);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
+            this.tabs.ResumeLayout(false);
+            this.tabList.ResumeLayout(false);
+            this.tabChart.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chartSeries)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -166,10 +166,10 @@
         }
 
         #endregion
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.TabPage tabList;
         private System.Windows.Forms.ListBox lbTransactions;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabChart;
         private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
         private System.Windows.Forms.DateTimePicker dateTimePickerStart;
         private System.Windows.Forms.Label lblEndDate;
