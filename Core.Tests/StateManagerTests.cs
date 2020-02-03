1166944db19cd35d8dd5ab1ee8c00c6a191f7426
Alina Siriak <adelhaide22@gmail.com>
2020-02-03 16:31:40 +0200

    Add test project

---
+++ Core.Tests/StateManagerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Core.Tests
+{
+    public class StateManagerTests
+    {
+        [Fact]
+        public void ReturnsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
