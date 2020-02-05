d6135d9d397fbed1ac605f5d9018331e482cbf51
Andrii Siriak <siryaka@gmail.com>
2020-02-05 12:20:52 +0200

    Cleanup

--- Core/PrivatTransactionsImporter.cs
+++ Core/PrivatTransactionsImporter.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
@@ -9,75 +8,89 @@ using System.Xml;
 
 namespace Core
 {
-    public class PrivatTransactionsImporter
-    {
-        public static async Task ImportTransactions(Credentials credentials, Action<List<Transaction>> handler)
-        {
-            for (var date = DateTime.Now.ToDate(); date.Year > 2015; date -= TimeSpan.FromDays(1))
-            {
-                handler(await LoadData(date, credentials));
-            }
-        }
+	public class PrivatTransactionsImporter
+	{
+		public static async Task ImportTransactions(Credentials credentials, Action<List<Transaction>> handler)
+		{
+			for (var date = DateTime.Now.ToDate(); date.Year > 2015; date -= TimeSpan.FromDays(1))
+			{
+				handler(await LoadData(date, credentials));
+			}
+		}
 
-        private static async Task<List<Transaction>> LoadData(Date date, Credentials credentials)
-        {           
-            var data = $"<oper>cmt</oper><wait>60</wait><payment><prop name=\"sd\" value=\"{date.Day}.{date.Month}.{date.Year}\"/><prop name=\"ed\" value=\"{date.Day}.{date.Month}.{date.Year}\"/><prop name=\"card\" value=\"{credentials.CardNumber}\"/></payment>";
-            var md5 = MD5.Create();
-            var sha1 = SHA1.Create();
+		private static async Task<List<Transaction>> LoadData(Date date, Credentials credentials)
+		{
+			var data =
+				$"<oper>cmt</oper><wait>60</wait><payment><prop name=\"sd\" value=\"{date.Day}.{date.Month}.{date.Year}\"/><prop name=\"ed\" value=\"{date.Day}.{date.Month}.{date.Year}\"/><prop name=\"card\" value=\"{credentials.CardNumber}\"/></payment>";
+			var md5 = MD5.Create();
+			var sha1 = SHA1.Create();
 
-            var secret = data + credentials.MerchantPassword;
-            var bytes = Encoding.UTF8.GetBytes(secret);
-            var md5h = md5.ComputeHash(bytes);
-            var md5hs = ByteArrayToString(md5h);
-            var sha1h = sha1.ComputeHash(Encoding.UTF8.GetBytes(md5hs));
-            var signature = ByteArrayToString(sha1h);
+			var secret = data + credentials.MerchantPassword;
+			var bytes = Encoding.UTF8.GetBytes(secret);
+			var md5h = md5.ComputeHash(bytes);
+			var md5hs = ByteArrayToString(md5h);
+			var sha1h = sha1.ComputeHash(Encoding.UTF8.GetBytes(md5hs));
+			var signature = ByteArrayToString(sha1h);
 
-            HttpClient client = new HttpClient();
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://api.privatbank.ua/p24api/rest_fiz"),
-                Content = new StringContent($"<?xml version=\"1.0\" encoding=\"UTF-8\"?><request version=\"1.0\"><merchant><id>{credentials.MerchantId}</id><signature>{signature}</signature></merchant><data>{data}</data></request>",
-                    Encoding.Default, "application/xml"),
-            };
-            var res = await client.SendAsync(request);
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://api.privatbank.ua/p24api/rest_fiz"),
+				Content = new StringContent(
+					$"<?xml version=\"1.0\" encoding=\"UTF-8\"?><request version=\"1.0\"><merchant><id>{credentials.MerchantId}</id><signature>{signature}</signature></merchant><data>{data}</data></request>",
+					Encoding.Default,
+					"application/xml")
+			};
+			var res = await client.SendAsync(request);
 
-            var xml = await res.Content.ReadAsStringAsync();
+			var xml = await res.Content.ReadAsStringAsync();
 
-            var xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(xml);
+			var xmlDoc = new XmlDocument();
+			xmlDoc.LoadXml(xml);
 
-            var xpath = "response/data/info/statements/statement";
-            var nodes = xmlDoc.SelectNodes(xpath);
-            
-            var list = new List<Transaction>();
-            foreach (XmlNode childrenNode in nodes)
-            {
-                list.Add(GetTransactionFromXml(childrenNode));
-            }
-            return list;
-        }
+			const string xpath = "response/data/info/statements/statement";
+			var nodes = xmlDoc.SelectNodes(xpath);
 
-        private static string ByteArrayToString(byte[] ba)
-        {
-            var hex = new StringBuilder(ba.Length * 2);
-            foreach (var b in ba)
-            {
-                _ = hex.AppendFormat("{0:x2}", b);
-            }
+			var list = new List<Transaction>();
+			foreach (XmlNode childrenNode in nodes)
+			{
+				list.Add(GetTransactionFromXml(childrenNode));
+			}
 
-            return hex.ToString();
-        }
+			return list;
+		}
 
-        private static Transaction GetTransactionFromXml(XmlNode node)
-        {
-            return new Transaction(node.Attributes["card"].Value,
-                node.Attributes["appcode"].Value,
-                DateTimeOffset.Parse(node.Attributes["trandate"].Value + " " + node.Attributes["trantime"].Value),
-                new Money(decimal.Parse(node.Attributes["cardamount"].Value.Split(' ').First()), (Currency)Enum.Parse(typeof(Currency), node.Attributes["cardamount"].Value.Split(' ').Last())),
-                new Money(decimal.Parse(node.Attributes["rest"].Value.Split(' ').First()), (Currency)Enum.Parse(typeof(Currency), node.Attributes["rest"].Value.Split(' ').Last())),
-                node.Attributes["terminal"].Value,
-                node.Attributes["description"].Value);
-        }
-    }
+		private static string ByteArrayToString(byte[] ba)
+		{
+			var hex = new StringBuilder(ba.Length * 2);
+			foreach (var b in ba)
+			{
+				_ = hex.AppendFormat("{0:x2}", b);
+			}
+
+			return hex.ToString();
+		}
+
+		private static Transaction GetTransactionFromXml(XmlNode node)
+		{
+			var cartNumber = node.Attributes["card"].Value;
+			var appCode = node.Attributes["appcode"].Value;
+			var timeStamp = DateTimeOffset.Parse(node.Attributes["trandate"].Value + " " +
+			                                     node.Attributes["trantime"].Value);
+			var amount = ParseMoney(node.Attributes["amount"]);
+			var rest = ParseMoney(node.Attributes["rest"]);
+			var terminal = node.Attributes["terminal"].Value;
+			var description = node.Attributes["description"].Value;
+			return new Transaction(cartNumber, appCode, timeStamp, amount, rest, terminal, description);
+		}
+
+		private static Money ParseMoney(XmlAttribute money)
+		{
+			var amountSplit = money.Value.Split(' ');
+			var amount = decimal.Parse(amountSplit[0]);
+			var currency = (Currency) Enum.Parse(typeof(Currency), amountSplit[1]);
+			return new Money(amount, currency);
+		}
+	}
 }
