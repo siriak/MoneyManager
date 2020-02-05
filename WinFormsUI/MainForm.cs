e2e9d3d5f443703b140e2d2b77c506aa2e87f111
Andrii Siriak <siryaka@gmail.com>
2020-02-05 12:11:55 +0200

    Cleanup

--- WinFormsUI/MainForm.cs
+++ WinFormsUI/MainForm.cs
@@ -1,91 +1,95 @@
 ﻿using System;
-using System.Linq;
-using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 using Core;
 
 namespace WinFormsUI
 {
-    public partial class MainForm : Form
-    {
-        Date startDate, endDate;
-        event Action OnFilteringUpdated = () => { };
+	public partial class MainForm : Form
+	{
+		private Date startDate, endDate;
 
-        public MainForm() => InitializeComponent();
+		public MainForm() => InitializeComponent();
+		private event Action OnFilteringUpdated = () => { };
 
-        private async void MainForm_Load(object sender, EventArgs e)
-        {
-            State.OnStateUpdated += RefreshList;
-            State.OnStateUpdated += RefreshChart;
-            OnFilteringUpdated += RefreshList;
-            OnFilteringUpdated += RefreshChart;
+		private async void MainForm_Load(object sender, EventArgs e)
+		{
+			State.OnStateUpdated += RefreshList;
+			State.OnStateUpdated += RefreshChart;
+			OnFilteringUpdated += RefreshList;
+			OnFilteringUpdated += RefreshChart;
 
-            dateTimePickerStart.Value = new DateTime(DateTime.Now.Year - 1, DateTime.Now.Month, DateTime.Now.Day);
-            dateTimePickerEnd.Value = DateTime.Now.Date;
+			dateTimePickerStart.Value = new DateTime(DateTime.Now.Year - 1, DateTime.Now.Month, DateTime.Now.Day);
+			dateTimePickerEnd.Value = DateTime.Now.Date;
 
-            clbCategories.Items.AddRange(State.Categories.ToArray());
-            clbCategories.SelectedIndexChanged += (o, e) => RefreshChart();
-            clbCategories.SelectedIndexChanged += (o, e) => RefreshList();
-            clbCategories.SetItemChecked(clbCategories.FindStringExact("All"), true);
+			clbCategories.Items.AddRange(State.Categories.ToArray());
+			clbCategories.SelectedIndexChanged += (o, e) => RefreshChart();
+			clbCategories.SelectedIndexChanged += (o, e) => RefreshList();
+			clbCategories.SetItemChecked(clbCategories.FindStringExact("All"), true);
 
-            await State.Init();
-        }
+			await State.Init();
+		}
 
-        private void RefreshList()
-        {
-            lbTransactions.Items.Clear();
+		private void RefreshList()
+		{
+			lbTransactions.Items.Clear();
 
-            lbTransactions.Items.AddRange(State.GetTransactionsUnion(clbCategories.CheckedItems.Cast<object>().Select(clbCategories.GetItemText), startDate, endDate)
-                .Select(t => (object)$"{t.Amount.Amount} {t.Amount.Currency}: {t.Description}")
-                .Reverse()
-                .ToArray());
-        }
+			lbTransactions.Items.AddRange(
+				State.GetTransactionsUnion(
+					      clbCategories.CheckedItems.Cast<object>().Select(clbCategories.GetItemText),
+					      startDate,
+					      endDate)
+				     .Select(t => (object) $"{t.Amount.Amount} {t.Amount.Currency}: {t.Description}")
+				     .Reverse()
+				     .ToArray());
+		}
 
-        private void RefreshChart()
-        {
-            chartSeries.Series.Clear();
+		private void RefreshChart()
+		{
+			chartSeries.Series.Clear();
 
-            var selectedCategories = clbCategories.CheckedItems.Cast<object>().Select(clbCategories.GetItemText).ToList();
-            var seriesToRemove = chartSeries.Series.Where(s => selectedCategories.All(c => c != s.Name)).ToList();
+			var selectedCategories = clbCategories.CheckedItems
+			                                      .Cast<object>()
+			                                      .Select(clbCategories.GetItemText)
+			                                      .ToList();
+			var seriesToRemove = chartSeries.Series.Where(s => selectedCategories.All(c => c != s.Name)).ToList();
 
-            foreach (var s in seriesToRemove)
-            {
-                chartSeries.Series.Remove(s);
-            }
-            
-            foreach (var c in selectedCategories)
-            {
-                if (chartSeries.Series.FindByName(c) is null)
-                {
-                    var newSeries = new Series
-                    {
-                        Name = c,
-                        ChartType = SeriesChartType.Line,
-                    };
-                    chartSeries.Series.Add(newSeries);
-                }
+			foreach (var s in seriesToRemove)
+			{
+				chartSeries.Series.Remove(s);
+			}
 
-                var series = chartSeries.Series.FindByName(c);
-                series.Points.Clear();
-                var timeSeries = State.GetTimeSeries(c, 0.99);
+			foreach (var c in selectedCategories)
+			{
+				if (chartSeries.Series.FindByName(c) is null)
+				{
+					var newSeries = new Series
+					{
+						Name = c,
+						ChartType = SeriesChartType.Line
+					};
+					chartSeries.Series.Add(newSeries);
+				}
 
-                for (var date = startDate; date <= endDate; date = date.AddDays(1))
-                {
-                    _ = series.Points.AddXY(date.ToString(), timeSeries[date]);
-                }
-            }
-        }
+				var series = chartSeries.Series.FindByName(c);
+				series.Points.Clear();
+				var timeSeries = State.GetTimeSeries(c, 0.99);
 
-        private void dateTimePickerStart_ValueChanged(object sender, EventArgs e)
-        {
-            startDate = dateTimePickerStart.Value.ToDate();
-            OnFilteringUpdated();
-        }
+				for (var date = startDate; date <= endDate; date = date.AddDays(1))
+				{
+					_ = series.Points.AddXY(date.ToString(), timeSeries[date]);
+				}
+			}
+		}
 
-        private void dateTimePickerEnd_ValueChanged(object sender, EventArgs e)
-        {
-            endDate = dateTimePickerEnd.Value.ToDate();
-            OnFilteringUpdated();
-        }
-    }
+		private void dateTimePickerStart_ValueChanged(object sender, EventArgs e)
+		{
+			startDate = dateTimePickerStart.Value.ToDate();
+			OnFilteringUpdated();
+		}
+
+		private void dateTimePickerEnd_ValueChanged(object sender, EventArgs e)
+		{
+			endDate = dateTimePickerEnd.Value.ToDate();
+			OnFilteringUpdated();
+		}
+	}
 }
--- WinFormsUI/Program.cs
+++ WinFormsUI/Program.cs
@@ -1,9 +1,7 @@
-using System.Windows.Forms;
-
 namespace WinFormsUI
 {
-    public class Program
-    {
-        public static void Main() => Application.Run(new MainForm());
-    }
+	public class Program
+	{
+		public static void Main() => Application.Run(new MainForm());
+	}
 }
