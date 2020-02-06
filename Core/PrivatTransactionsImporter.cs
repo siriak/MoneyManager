5904d3ceb26752fbef9c6c8688977a1ae0218ee8
Andrii Siriak <siryaka@gmail.com>
2020-02-06 15:08:11 +0200

    Cleanup

--- Core/PrivatTransactionsImporter.cs
+++ Core/PrivatTransactionsImporter.cs
@@ -73,7 +73,7 @@ namespace Core
 			var cartNumber = node.Attributes["card"].Value;
 			var appCode = node.Attributes["appcode"].Value;
 			var timeStamp = DateTimeOffset.Parse(node.Attributes["trandate"].Value + " " + node.Attributes["trantime"].Value);
-			var amount = ParseMoney(node.Attributes["amount"]);
+			var amount = ParseMoney(node.Attributes["cardamount"]);
 			var rest = ParseMoney(node.Attributes["rest"]);
 			var terminal = node.Attributes["terminal"].Value;
 			var description = node.Attributes["description"].Value;
