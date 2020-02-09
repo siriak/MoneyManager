7cd27179a693f0b6e33ae05553bc74108e02427b
Alina Siriak <35349311+Adelhaide22@users.noreply.github.com>
2020-02-09 12:31:49 +0200

    Add rules (#23)

---
+++ Core/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Core
+{
+    public class Category
+    {
+		public Category(string name, ICollection<Rule> rules)
+		{
+			Name = name;
+			Rules = rules;
+		}
+
+		public string Name { get; set; }
+		public ICollection<Rule> Rules { get; }
+	}
+}
---
+++ Core/Rule.cs
@@ -0,0 +1,21 @@
+﻿namespace Core
+{
+    public class Rule
+    {
+		public string CardNumber { get; }
+		public string IsIncome { get; }
+		public string IsExpence { get; }
+		public string Terminal { get; }
+		public string Description { get; }
+
+		public Rule(string cardNumber, string isIncome,
+			string isExpence, string terminal, string description)
+		{
+			CardNumber = cardNumber;
+			IsIncome = isIncome;
+			IsExpence = isExpence;
+			Terminal = terminal;
+			Description = description;
+		}
+	}
+}
