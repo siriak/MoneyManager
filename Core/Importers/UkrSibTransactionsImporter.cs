5fd72e192d1bbde8c90989518ca99b53e90ef128
Alina Siriak <adelhaide22@gmail.com>
2020-07-31 09:48:04 +0300

    Refactor

--- Core/Importers/UkrSibTransactionsImporter.cs
+++ Core/Importers/UkrSibTransactionsImporter.cs
@@ -12,25 +12,33 @@ namespace Core.Importers
     {
         public IList<Transaction> Load(Stream file)
         {
-            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            using var reader = ExcelReaderFactory.CreateOpenXmlReader(file);
-            var dataSet = reader.AsDataSet();
-            var dataTable = dataSet.Tables[0];
-
+            var saved = Thread.CurrentThread.CurrentCulture;
             var transactions = new List<Transaction>();
-            for (var i = 1; i < dataTable.Rows.Count; i++)
+
+            try
             {
-                var date = dataTable.Rows[i][1].ToString();
-                var category = dataTable.Rows[i][4].ToString();
-                var cardNumber = dataTable.Rows[i][3].ToString();
-                var description = dataTable.Rows[i][2].ToString().Trim();
-                var amount = dataTable.Rows[i][5].ToString();
-                var currency = dataTable.Rows[i][6].ToString();
-                transactions.Add(new Transaction(cardNumber, Date.Parse(date),
-                    new Money(double.Parse(amount), MoneyManager.ParseCurrency(currency)), description, category));
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                using var reader = ExcelReaderFactory.CreateOpenXmlReader(file);
+                var dataSet = reader.AsDataSet();
+                var dataTable = dataSet.Tables[0];
+                for (var i = 1; i < dataTable.Rows.Count; i++)
+                {
+                    var date = dataTable.Rows[i][1].ToString();
+                    var category = dataTable.Rows[i][4].ToString();
+                    var cardNumber = dataTable.Rows[i][3].ToString();
+                    var description = dataTable.Rows[i][2].ToString().Trim();
+                    var amount = dataTable.Rows[i][5].ToString();
+                    var currency = dataTable.Rows[i][6].ToString();
+                    transactions.Add(new Transaction(cardNumber, Date.Parse(date),
+                        new Money(double.Parse(amount), MoneyManager.ParseCurrency(currency)), description, category));
+                }
             }
+            finally
+            {
+                Thread.CurrentThread.CurrentCulture = saved;
+            }            
 
             return transactions;
         }
