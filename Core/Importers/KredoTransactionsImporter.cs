d63c3f9054bacbd98ed98cb329a84ededd4e24a4
Alina Siriak <adelhaide22@gmail.com>
2020-08-04 20:22:48 +0300

    Refactor

--- Core/Importers/KredoTransactionsImporter.cs
+++ Core/Importers/KredoTransactionsImporter.cs
@@ -1,17 +1,10 @@
-﻿using iText.IO.Font;
-using iText.Kernel;
-using iText.Kernel.Font;
-using iText.Kernel.Pdf;
+﻿using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser;
 using iText.Kernel.Pdf.Canvas.Parser.Listener;
 using iText.Layout;
-using iText.IO.Font.Constants;
-using iText.Layout.Element;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace Core.Importers
 {
@@ -36,8 +29,8 @@ namespace Core.Importers
 
                 while (actualText.Length != 0)
                 {
-                    var transactionData = actualText.Skip(2).TakeWhile(s => Date.TryParse(s, out Date d) == false).ToArray();
-                    if (Date.TryParse(transactionData.Last().Trim('/'), out Date d) == true)
+                    var transactionData = actualText.Skip(2).TakeWhile(s => !Date.TryParse(s, out Date d)).ToArray();
+                    if (Date.TryParse(transactionData.Last().Trim('/'), out Date d))
                     {
                         transactionData = transactionData.SkipLast(1).ToArray();
                     }
@@ -46,7 +39,7 @@ namespace Core.Importers
                     var category = string.Empty;
                     var cardNumber = string.Empty;
                     var description = string.Concat(transactionData.Skip(1)
-                        .TakeWhile(s => double.TryParse(s.Split(' ')[0], out double d) == false));
+                        .TakeWhile(s => !double.TryParse(s.Split(' ')[0], out double d)));
                     var amount = transactionData.Last().Split(' ')[0];
                     var currency = transactionData.Last().Split(' ')[1];
                     transactions.Add(new Transaction(cardNumber, Date.Parse(date),
