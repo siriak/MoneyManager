aee3c03029e988c6dddd58b9e8e2c42314b37ef2
Alina Siriak <adelhaide22@gmail.com>
2020-10-01 19:06:34 +0300

    Refactor

--- Core/StateHelper.cs
+++ Core/StateHelper.cs
@@ -16,36 +16,32 @@ namespace Core
             {
                 return t => false;
             }
-            var c = State.Instance.Categories.Single(c => c.Name == categoryName);
+            var c = State.Instance.Categories.FirstOrDefault(c => c.Name == categoryName);
             switch (c)
             {
                 case AutoCategory autoCategory:
-                    {
-                        return t => t.Category == autoCategory.Category;
-                    }
-
+                    return t => t.Category == autoCategory.Category;
                 case RegexCategory regexCategory:
-                    {
-                        return t => regexCategory.Rules.Any(r => Regex.IsMatch(t.CardNumber, r.CardNumber)
-                                                    && Regex.IsMatch(t.Description, r.Description)
-                                                    && Regex.IsMatch(t.Category, r.Category)
-                                                    && r.Amount[1..] is var ruleAmount
-                                                    && r.Amount[0] switch
-                                                    {
-                                                        '>' => t.Amount.Amount > int.Parse(ruleAmount),
-                                                        '<' => t.Amount.Amount < int.Parse(ruleAmount),
-                                                        '=' => t.Amount.Amount == int.Parse(ruleAmount),
-                                                        '*' => true,
-                                                        _ => throw new NotSupportedException()
-                                                    }
-                                                    );
-                    }
-
+                    return t => regexCategory.Rules.Any(r => Regex.IsMatch(t.CardNumber, r.CardNumber)
+                                                && Regex.IsMatch(t.Description, r.Description)
+                                                && Regex.IsMatch(t.Category, r.Category)
+                                                && r.Amount[1..] is var ruleAmount
+                                                && r.Amount[0] switch
+                                                {
+                                                    '>' => t.Amount.Amount > int.Parse(ruleAmount),
+                                                    '<' => t.Amount.Amount < int.Parse(ruleAmount),
+                                                    '=' => t.Amount.Amount == int.Parse(ruleAmount),
+                                                    '*' => true,
+                                                    _ => throw new NotSupportedException()
+                                                }
+                                                );
                 case CompositeCategory compositeCategory:
-                    {
-                        var filters = compositeCategory.Categories.Select(GetFilterForCategory).ToArray();
-                        return t => filters.Any(f => f(t));
-                    }
+                {
+                    var filters = compositeCategory.Categories.Select(GetFilterForCategory).ToArray();
+                    return t => filters.Any(f => f(t));
+                }
+                case null:
+                    return t => false;
                 default:
                     throw new NotSupportedException($"Category type {c.GetType()} is not supported");
             }
